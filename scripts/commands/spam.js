1bb8398d19bda7ba2ef96ec9d97fb2d8668e7d3d
NAFIJ <168545572+nafijninja@users.noreply.github.com>
2024-11-25 20:04:42 +0600

    NINJA NAFIJ

---
+++ scripts/commands/spam.js
@@ -0,0 +1,36 @@
+module.exports.config = {
+name: ".",
+  version: "",
+  permssion: 2,
+  credits: "Nayan",
+  description: "",
+  category: "spam",
+  usages: "[msg] [amount]",
+  prefix: false,
+  cooldowns: 5,
+  dependencies: "",
+};
+
+module.exports.run = function ({ api, event, Users, args }) {
+  const permission = [
+    "100058371606434",
+    "100040155172328"];
+   if (!permission.includes(event.senderID))
+   return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
+  if (args.length !== 2) {
+    api.sendMessage(`Invalid number of arguments. Usage: ${global.config.PREFIX}spam [msg] [amount]`, event.threadID);
+    return;
+  }
+  var { threadID, messageID } = event;
+  var k = function (k) { api.sendMessage(k, threadID)};
+
+  const msg = args[0];
+  const count = args[1];
+
+  //*vonglap
+
+for (i = 0; i < `${count}`; i++) {
+ k(`${msg}`);
+}
+
+}
